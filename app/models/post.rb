fe2bb4b13c51bdcab1fced9928d7cd5ca3ff3510
Clayton Sweeten <cjsweeten101@gmail.com>
2017-09-28 19:38:18 -0600

    Posts completed with validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
-	
+	validates :title, presence: true, length: { maximum: 140 }
+	LINK_REGEX = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+	validates :link, presence: true, format: { with: LINK_REGEX, multiline: true }
+	validates :user_id, presence: true
 end
--- db/migrate/20170929003428_create_posts.rb
+++ db/migrate/20170929003428_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
-    	t.belongs_to :user, index: true
+    	t.belongs_to :user, foriegn_key: true, index: true
       t.string :link
       t.string :title
       t.timestamps
