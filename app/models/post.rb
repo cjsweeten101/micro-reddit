7b0081962eecac4150ab60c2e55f07f430c06c3e
Clayton Sweeten <cjsweeten101@gmail.com>
2017-09-28 19:58:30 -0600

    added assocations in model files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
+	has_many :comments
+	belongs_to :user
+
 	validates :title, presence: true, length: { maximum: 140 }
 	LINK_REGEX = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
 	validates :link, presence: true, format: { with: LINK_REGEX, multiline: true }
