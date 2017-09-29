22c894e4c57b7615e7b59908954a8a1ec8dd42a3
Clayton Sweeten <cjsweeten101@gmail.com>
2017-09-28 18:19:40 -0600

    Changes to secure passwords

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 20170928235137) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
